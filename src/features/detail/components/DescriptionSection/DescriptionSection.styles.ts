a8b9a5e342bc059b28d9674b0ec1a8d4e5c2da66
Henil Patel <henil20patel@gmail.com>
2026-07-17 17:27:03 +0530

    feat(details): add reusable DescriptionSection component

---
+++ src/features/detail/components/DescriptionSection/DescriptionSection.styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: { paddingHorizontal: 20, marginBottom: 28 },
+  title: { marginBottom: 12, fontWeight: "700" },
+  description: { lineHeight: 24 },
+  readMore: { marginTop: 12, alignSelf: "flex-start" },
+});
