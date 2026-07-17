ad1f767b0fd22fb258bd27c6b6316b2a9065402e
Henil Patel <henil20patel@gmail.com>
2026-07-17 20:29:58 +0530

    feat(profile): add reusable ProfileHeader component

--- src/features/profile/ProfileScreen.tsx
+++ src/features/profile/ProfileScreen.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ProfileHeader from "./components/ProfileHeader";
 export default function ProfileScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-xl">
-        Profile Screen
-      </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      
+      <ProfileHeader
+        name="Henil Patel"
+        role="React Native Developer"
+        email="henil@example.com"
+        onEditProfile={() => console.log("Edit Profile")}
+      />
+    </SafeAreaView>
   );
 }
---
+++ src/features/profile/components/ProfileHeader/ProfileHeader.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 32,
+  },
+  avatar: { width: 100, height: 100, borderRadius: 50, marginBottom: 20 },
+  name: { fontWeight: "700", marginBottom: 4 },
+  role: { marginBottom: 4 },
+  email: { marginBottom: 20 },
+  button: { width: 180 },
+});
